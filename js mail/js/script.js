20a8fb3f6abee9312acbc799c4cfe1ed5ba86cb3
Matpas97 <passarellim22@gmail.com>
2022-06-08 18:02:06 +0200

    Mail

--- js mail/js/script.js
+++ js mail/js/script.js
@@ -6,9 +6,27 @@
 
 // Chiedo all'utente la sua mail 
 
-let userMail = prompt("Qual'è la tua mail ?");
+const userMail = prompt("Qual'è la tua mail ?");
 
 // Lista user
 
-const listMail = ['mario.rossi@gmail.com','pippo-mauro@libero.com','francopluto@gmail.com','pincopallo99@gmail.com'];
-console.log(listMail);
+const listLogin = ['mario.rossi@gmail.com','pippo-mauro@libero.com','francopluto@gmail.com','pincopallo99@gmail.com','passarellim@gmail.com'];
+ 
+
+ // Controllo se la mia mail è nella lista list Login
+
+ let mailFound = false;
+ for(let i = 0; i < listLogin.length; i++) {
+     const thisMail = listLogin[i];
+
+     if(thisMail === userMail) {
+         mailFound = true;
+     }
+ }
+// Se la mail user è presente nell'lista array alora dico allo user che  presente altrimenti non lo è
+ if(mailFound) {
+     alert('La sua mail è presente nella lista userMail ');
+ } 
+ else {
+     alert('La sua mail non è presente nella lista userMail');
+ } 
