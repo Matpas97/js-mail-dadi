227585eca67c9456669c4a3eea17f83ea77d07b2
Matpas97 <passarellim22@gmail.com>
2022-06-08 17:46:08 +0200

    first commit

---
+++ js mail/js/script.js
@@ -0,0 +1,14 @@
+//Mail
+//Chiedi all’utente la sua email,
+//controlla che sia nella lista di chi può accedere,
+//stampa un messaggio appropriato sull’esito del controllo.
+
+
+// Chiedo all'utente la sua mail 
+
+let userMail = prompt("Qual'è la tua mail ?");
+
+// Lista user
+
+const listMail = ['mario.rossi@gmail.com','pippo-mauro@libero.com','francopluto@gmail.com','pincopallo99@gmail.com'];
+console.log(listMail);
